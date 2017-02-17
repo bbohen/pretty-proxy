c71dcbd47766505db548309629f19bde871de0a4
Brad Bohen <bbohen@gmail.com>
2017-02-16 23:49:53 -0500

    SSL traffic proxy working

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
+const net = require('net');
 const httpProxy = require('http-proxy');
 
-const proxy = httpProxy.createProxyServer({ agent: http.agent });
-
-proxy.on('error', function (err, req, res) {
-  console.log(err);
-});
-
-// proxy.on('proxyRes', function (proxyRes, req, res) {
-//   var key = 'www-authenticate';
-//   proxyRes.headers[key] = proxyRes.headers[key] && proxyRes.headers[key].split(',');
-// });
-
+const proxy = httpProxy.createProxyServer();
 const server = http.createServer(function(req, res) {
+  console.log('************************************* Request!');
   console.log(req.url);
+  console.log('**********************************************');
   proxy.web(req, res, {
     target: req.url,
     prependPath: false,
-    secure: false,
   }, (err) => {
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Proxy Error!');
     console.log(err);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Mistakes were made: ' + req.url + '\n' + JSON.stringify(err, true, 2));
-    res.end();
-  });
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  })
 });
 
-server.listen(5050);
-console.log("listening on port 5050");
+server.on('connect', (req, socket, head) => {
+  console.log('******************************* HTTPS Request!');
+  console.log(req.url);
+  console.log('**********************************************');
+
+  const parts = req.url.split(':', 2);
+	// open a TCP connection to the remote host
+	const conn = net.connect(parts[1], parts[0], function() {
+		// respond to the client that the connection was made
+		socket.write("HTTP/1.1 200 OK\r\n\r\n");
+		// create a tunnel between the two hosts
+		socket.pipe(conn);
+		conn.pipe(socket);
+	});
+});
+
+server.on('error', function (err, req, res) {
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Server Error!');
+  console.log(err);
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+});
+
+server.listen(5060);
+console.log("listening on port 5060");
