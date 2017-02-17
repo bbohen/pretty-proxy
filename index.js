3710b8b089af397cf966ae2a9dfc569be8e1a528
Brad Bohen <bbohen@gmail.com>
2017-02-17 00:27:41 -0500

    Basic error handling

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const https = require('https');
 const net = require('net');
 const httpProxy = require('http-proxy');
 
+process.on('uncaughtException', (err) => {
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Uncaught Error!');
+  console.log(err);
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+});
+
 const proxy = httpProxy.createProxyServer();
 const server = http.createServer(function(req, res) {
   console.log('************************************* Request!');
   console.log(req.url);
   console.log('**********************************************');
   proxy.web(req, res, {
-    target: req.url,
+    changeOrigin: true,
     prependPath: false,
+    target: req.url,
   }, (err) => {
-    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Proxy Error!');
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!! Proxy Request Error!');
     console.log(err);
     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
   })
@@ -26,20 +33,14 @@ server.on('connect', (req, socket, head) => {
 
   const parts = req.url.split(':', 2);
 	// open a TCP connection to the remote host
-	const conn = net.connect(parts[1], parts[0], function() {
-		// respond to the client that the connection was made
-		socket.write("HTTP/1.1 200 OK\r\n\r\n");
-		// create a tunnel between the two hosts
-		socket.pipe(conn);
-		conn.pipe(socket);
-	});
-});
-
-server.on('error', function (err, req, res) {
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Server Error!');
-  console.log(err);
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-});
+  const conn = net.connect(parts[1], parts[0], () => {
+    // respond to the client that the connection was made
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      // create a tunnel between the two hosts
+      socket.pipe(conn);
+      conn.pipe(socket);
+    })
+  });
 
 server.listen(5060);
 console.log("listening on port 5060");
