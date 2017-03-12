8cfab83d126a3122d3e78ff9a1a469696ece78a1
Brad Bohen <bbohen@gmail.com>
2017-03-12 16:24:03 -0400

    Adding ssl connections to the socket broadcasts

--- src/server/withSockets.js
+++ src/server/withSockets.js
@@ -1,6 +1,18 @@
 const server = require('./index.js');
 const io = require('socket.io')(server);
 
+function emitRequest({ statusCode, url }) {
+  const isSocketIo = url.startsWith('/socket.io/');
+
+  // ignore the requests to socket.io itself
+  if (!isSocketIo) {
+    io.emit('request', {
+      statusCode,
+      url,
+    });
+  }
+}
+
 io.on('connection', () => {
   console.log('socket connected');
 });
@@ -9,18 +21,8 @@ io.on('error', () => {
   console.log('socket error');
 });
 
-server.on('request', (data) => {
-  console.log('request');
-  const isSocketIo = data.url.startsWith('/socket.io/');
-
-  // ignore the requests to socket.io itself
-  if (!isSocketIo) {
-    io.emit('request', {
-      statusCode: data.statusCode,
-      url: data.url,
-    });
-  }
-});
+server.on('connect', emitRequest);
+server.on('request', emitRequest);
 
 // move this to index
 process.on('uncaughtException', () => {
